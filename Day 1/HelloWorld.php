0aef0008027fdc3d30e09a115585a03a2b87760c
Ben B <btb4516@g.rit.edu>
2025-08-27 10:14:34 -0400

    fix folder stuff

--- Day 1/HelloWorld.php
+++ Day 1/HelloWorld.php
@@ -4,13 +4,20 @@
     $title = "My first PHP Program";
     //one line comment
     /*
-        multiline   
+        multiline
         comment
     */
 ?>
     <h1><?php echo "<p>Hi World! -$title</p>"; ?></h1>
-    <?php 
+    <?php
         echo "<br />Name is ".$_GET['name']."<br />";
+
+        //phpinfor();
+
+        $version = phpversion();
+        echo "<h2> The version is: $version</h2>";
+
+        var_dump($_REQUEST);
     ?>
     </body>
 </html>
