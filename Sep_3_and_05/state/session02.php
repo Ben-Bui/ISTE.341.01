72e351ffb21abf4425f416b452a9d79df332477c
Ben B <btb4516@g.rit.edu>
2025-09-08 10:19:04 -0400

    session destroy part added

--- Sep_3_and_05/state/session02.php
+++ Sep_3_and_05/state/session02.php
@@ -1,5 +1,5 @@
 <?php
-session_name("Noble");
+session_name("Noble6");
 session_start();
 
 ?>
@@ -15,13 +15,27 @@ session_start();
         echo "Hi, {$_SESSION['name']} from 
             {$_SESSION['school']}. <br />>
             See, I remembered your name!";
-        echo "<a href='session01.php'>Page 1</a>"
+
+        unset($_SESSION['name']);//unsestting the 'name' session variable
+        session_unset(); //unset all session variables but doesn't terminate the session
+
+        //invalidate the session cookie ( with the session id)
+        if(isset($_COOKIE[session_name()])){
+            //the session cookie exists
+            $params = session_get_cookie_params();
+            setcookie(session_name(),'',1,$params['path'],
+            $params['domain'],$params['secure'],$params['httponly']);
+        }
+
+        session_destroy();// actually destop session itself
+        
+        echo "<a href='session01.php'>Page 1</a>";
     }else{
 ?>
     <p>Sorry,I dont't know you!</p>
     <a href='session01.php'>Page 1</a>
 <?php
-}
+    }
 
 ?>
     </body>
