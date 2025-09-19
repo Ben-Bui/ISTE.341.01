6700d703cefff77e9fe4f48d0f36fee3e9ded688
Ben B <btb4516@g.rit.edu>
2025-09-19 11:02:13 -0400

    class Friday sep 19

--- PHP/mysqliexamples/DB.class.php
+++ PHP/mysqliexamples/DB.class.php
@@ -39,9 +39,111 @@ class DB {
     }//getAllPeople
 
     function getAllPeopleAsTable() {
-
+       
         $data = $this->getAllPeople();
+
+        if (count($data) > 0) {
+       
+            $bigString = "<table border='1'>\n
+            <tr><th>ID</th>
+                <th>First</th>
+                <th>Last</th>
+                <th>Nick</th></tr>\n";
+
+        foreach ($data as $row) {
+            $bigString .= "<tr>
+                            <td><a href='phones.php?id={$row['id']}'>{$row['id']}</a></td>
+                            <td>{$row['first']}</td>
+                            <td>{$row['last']}</td>
+                            <td>{$row['nick']}</td>
+                        </tr>\n";
+        }
+
+            $bigString .= "</table>\n";
+       
+        } else {
+            $bigString = "<h2>No people exist.</h2>\n";
+        }
+
+        return $bigString;
 
     }//getAllPeopleAsTable
 
+    function insert($last,$first,$nick) {
+        
+        $query = "INSERT INTO people (Lastname, Firstname, Nickname) VALUES (?,?,?)";
+        $insertID = -1;
+
+        if ($stmt = $this->conn->prepare($query)) {
+            $stmt->bind_param("sss",$last,$first,$nick);
+            $stmt->execute();
+            $stmt->store_result();
+            $insertID = $stmt->insert_id;
+        }
+
+        return $insertID;
+    
+    }//insert
+
+        function update($fields) {
+            $query = "UPDATE people SET ";
+            $items = [];
+            $types = "";
+            $updateID = 0;
+
+            foreach ($fields as $k => $v) {
+                switch($k){
+                    case "nick":
+                        $query .= "NickName = ?, ";
+                        $items[] = $v;
+                        $types .= "s";
+                        break;
+                    case "first":
+                        $query .= "FirstName = ?, ";
+                        $items[] = $v;
+                        $types .= "s";
+                        break;
+                    case "last":
+                        $query .= "LastName = ?, ";
+                        $items[] = $v;
+                        $types .= "s";
+                        break;
+                    case "id":
+                        $updateID = intval($v);
+                        break;
+                    default:
+                        // ignore unknown fields
+                        break;
+                }
+            }
+
+            $query = rtrim($query, ", "); // remove trailing comma
+            $query .= " WHERE PersonID = ?";
+            $types .= "i";
+            $items[] = $updateID;
+
+            $numRows = 0;
+            if ($stmt = $this->conn->prepare($query)) {
+                $stmt->bind_param($types, ...$items);
+                $stmt->execute();
+                $numRows = $stmt->affected_rows;
+            }
+
+            return $numRows;
+
+        }//update
+
+        function delete($id) {
+            $query = "DELETE FROM people WHERE PersonID = ? ";
+            $numRows = 0;
+            if ($stmt = $this->conn->prepare($query)){
+                $stmt->bind_param("i", $id);
+                $stmt->execute();
+                $stmt->store_result();
+                $numRows = $stmt->affected_rows;
+        }
+
+            return $numRows;
+
+    }//delete
 }//DB
---
+++ PHP/mysqliexamples/people.php
@@ -0,0 +1,6 @@
+<?php
+    require_once("DB.class.php");
+
+    $db = new DB();
+    echo $db->getAllPeopleAsTable();
+?>
