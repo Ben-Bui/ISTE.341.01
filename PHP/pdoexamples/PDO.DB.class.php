5df1c7a85feb4c9b9858dfe3434ab08cea92a996
Ben B <btb4516@g.rit.edu>
2025-09-24 10:26:22 -0400

    class sep 24

--- PHP/pdoexamples/PDO.DB.class.php
+++ PHP/pdoexamples/PDO.DB.class.php
@@ -37,4 +37,84 @@ class DB {
         return $data;
 
     }//getperson
+
+    function getPersonAlt($id){
+       
+        $data = [];
+
+        try {
+
+            $stmt = $this->dbh->prepare("SELECT * FROM people WHERE PersonID = :id");
+            $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+            $stmt->execute();
+            while ($row = $stmt->fetch()){
+                $data[] = $row;
+            }
+
+        } catch(PDOException $pe){
+            echo $pe->getMessage();
+        }
+
+        return $data;
+
+    }//getperson
+
+    function getPersonAlt2($id){
+
+         $data = [];
+
+        try {
+
+            $stmt = $this->dbh->prepare("SELECT * FROM people WHERE PersonID = :id");
+            $stmt->execute(["id"=>$id]);
+
+            $data = $stmt->fetchAll();
+
+        } catch(PDOException $pe){
+            echo $pe->getMessage();
+        }
+
+        return $data;
+
+    }//getperson alt2
+
+    function insert($last,$first,$nick){
+
+        try {
+
+            $stmt = $this->dbh->prepare("INSERT INTO people(LastName, FirstName, NickName)
+                VALUES (:lastName, :firstName, :nickName)");
+
+            $stmt->execute(["lastName"=>$last, "firstName"=>$first, "nickName"=>$nick]);
+
+            return $this->dbh->lastInsertId();
+
+        } catch(PDOException $e){
+            echo $e->getMessage();
+            return -1;
+        }
+
+    }//insert
+
+    function getAllObjects() {
+
+        $data = [];
+
+        include "Person.class.php";
+
+        try {
+
+            $stmt = $this->dbh->prepare("SELECT * FROM people ");
+            $stmt->execute(["id"=>$id]);
+            while ($person = $stmt->fetch()){
+                $data[] = $row;
+            }
+
+        } catch(PDOException $pe){
+            echo $pe->getMessage();
+        }
+
+        return $data;
+    }//getAllObject
+
 }//DB
---
+++ PHP/pdoexamples/peoplePdo.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "PDO.DB.class.php";
+
+$db = new DB();
+
+$data = $db->getPerson(1);
+var_dump($data);
+foreach($data as $row) {
+    print_r($row);
+}
+echo "<hr />";
+
+$data = $db->getPersonAlt(1);
+
+foreach($data as $row) {
+    print_r($row);
+}
+echo "<hr />";
+
+$data = $db->getPersonAlt2(1);
+
+foreach($data as $row) {
+    print_r($row);
+}
+echo "<hr />";
+?>
