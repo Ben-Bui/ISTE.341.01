b7477fed102ca024ffa104698061b88f97d8ab28
Ben B <btb4516@g.rit.edu>
2025-10-06 21:08:23 +0000

    calss friday

---
+++ javaandjsondemo/src/main/java/com/bui/ben/Park.java
@@ -0,0 +1,13 @@
+package com.bui.ben;
+
+public class Park {
+    public String parkName;
+    public String parkLocation;
+    public String parkDescription;
+
+    public Park(String name, String location, String desc){
+        parkName = name;
+        parkDescription = desc;
+        parkLocation = location;
+    }
+}
---
+++ javaandjsondemo/src/main/java/com/bui/ben/Parks.java
@@ -0,0 +1,5 @@
+package com.bui.ben;
+
+public class Parks {
+    
+}
