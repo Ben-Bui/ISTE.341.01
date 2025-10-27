08cebd6b3dd1420b42526488c76821cd2493b747
Ben B <btb4516@g.rit.edu>
2025-10-27 10:49:11 -0400

    rest stuff

---
+++ rest/handlers.php
@@ -0,0 +1,27 @@
+<?php 
+
+//normally interact with a DB, hard-coding for now
+//have a function for each route
+//post
+function getProductsHandler($id="") {
+
+}//getProductsHandler
+
+//put
+function createProductHandler() {
+
+}//create Product
+
+//put
+function updateProductsHandler($id="") {
+
+}//updateProductsHandler
+
+//delete
+function deleteProductsHandler($id="") {
+
+}//deleteProductsHandler
+
+fucntion findElementByKeyValue($array,$key,$value){
+
+}//findElementByKeyValue
