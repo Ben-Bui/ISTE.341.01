dbb9844b7e2ef2b80af142d53ec2e2d3f613d671
Ben B <btb4516@g.rit.edu>
2025-10-31 10:48:27 -0400

    product service

--- dotnetdemos/ProductService/Program.cs
+++ dotnetdemos/ProductService/Program.cs
@@ -1,3 +1,14 @@
+using System.Collections.Generic;
+using ProductService.Models;
+
+var products = new List<Product>();
+
+for (int i=1; i<=5; i++)
+{
+    products.Add(new Product() {Name = "Product " + i, Id = i});
+}
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,30 +23,99 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/",()=> "Hello World");
+
+app.MapGet("/Product", () =>  products);
+
+app.MapGet("/Product/{id}", (int id) =>  
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    Product product = products.Find(x => x.Id == id); 
+    
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(product);
+});
+
+app.MapPut("/Product", (Product product) =>  
+{
+    //validation
+    bool isValid = !String.IsNullOrWhiteSpace(product.Name) &&
+            product.Name.Length < 40 ? true : false;
+   
+    if(product == null || !isValid)
+    {
+        return Results.BadRequest();
+    }
+
+    //normally insert into db and return a valid link
+    product.Id = 6;
+    return Results.Created($"Product/{product.Id}", product);
+});
+
+app.MapPost("/Product", (int id, Product product) =>  
+{
+    //validation
+    bool isValid = !String.IsNullOrWhiteSpace(product.Name) &&
+            product.Name.Length < 40 ? true : false;
+   
+    if(product == null || !isValid)
+    {
+        return Results.BadRequest();
+    }
+
+    if(!products.Exists( x => x.Id == id))
+    {
+        return Results.NotFound();
+    }
+    
+    //normally update
+    product.Id = 6;
+    return Results.NoContent();
+});
+
+app.MapDelete("/Product/{id}", (int id) =>  
+{
+    
+    if(!products.Exists( x => x.Id == id))
+    {
+        return Results.NotFound();
+    }
+    
+    //normally insert into db and return a valid link
+
+    return Results.NoContent();
+});
+
+app.MapGet("/Product2/{id}", (int id) =>  
+{
+    Product product = products.Find(x => x.Id == id)!;
+    
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    //if properties of object are private, can't return object like in /Product/{id}
+    //or will get an empty json{}, so need to build a dictionary and send that back
+    var returnPod = new Dictionary<string,object>
+    {
+            {"id",product.Id},
+            {"name",product.Name}
+    };
+
+    return Results.Ok(returnPod);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
