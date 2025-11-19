2600c1d0f6fc7c6414a4ebcdb7d36796df5f5919
Ben B <btb4516@g.rit.edu>
2025-11-19 10:29:59 -0500

    stuff

--- dotnetdemos/ProductService/obj/Debug/net9.0/ProductService.AssemblyInfo.cs
+++ dotnetdemos/ProductService/obj/Debug/net9.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accdcbf1130973b11c3341a263e290c8b91f852c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1326d33df328b6e9e268000527de1567380494")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
